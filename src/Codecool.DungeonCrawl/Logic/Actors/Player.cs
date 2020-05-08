6bb1d93d21823a85bd293f377bd026ab54667dc1
h0axx <55976481+h0axx@users.noreply.github.com>
2020-05-08 09:42:13 +0200

    player move override

--- src/Codecool.DungeonCrawl/Logic/Actors/Player.cs
+++ src/Codecool.DungeonCrawl/Logic/Actors/Player.cs
@@ -17,5 +17,24 @@ namespace Codecool.DungeonCrawl.Logic.Actors
 
         /// <inheritdoc/>
         public override string Tilename => "player";
+
+        /// <summary>
+        /// Moves player by the given amount
+        /// </summary>
+        /// <param name="dx">X amoount</param>
+        /// <param name="dy">Y amount</param>
+        public override void Move(int dx, int dy)
+        {
+            Cell nextCell = Cell.GetNeighbor(dx, dy);
+            if (nextCell.Tilename == "Floor")
+            {
+                if (nextCell.Actor == null)
+                {
+                    Cell.Actor = null;
+                    nextCell.Actor = this;
+                    Cell = nextCell;
+                }
+            }
+        }
     }
 }
